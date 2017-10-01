777bc0248b4c0eb301a46754b20a77d70357b0a7
segun s akowonjo <ssa39@afsaccess1.njit.edu>
2017-10-01 15:52:45 -0400

    I did my homework

--- index.php
+++ index.php
@@ -7,6 +7,26 @@ echo "The value of \$tar: ".$tar."<br>";
 
 $year = array("2012", "396", "300","2000", "1100", "1089");
 echo "The value of \$year: ";
-print_r($year)
-
+print_r($year);
+echo "<br>";
+$date = str_replace("-", "/", $date);
+echo $date;
+echo "<br>";
+if (strcmp($date, $tar)>0) {echo"the future";}
+elseif (strcmp($date, $tar)<0) {echo "the past";}
+else {echo "Oop";}
+echo "<br>";
+echo strpos($date, "/")." ".strrpos($date, "/");
+echo "<br><br>";
+echo str_word_count($date);
+echo "<br><br>";
+echo strlen($date);
+echo "<br><br>";
+echo ord($date);
+echo "<br><br>";
+echo substr($date, -2);
+echo "<br><br>";
+$date = explode("/",$date);
+foreach ($date as $value) {echo $value." ";
+}
 ?>
